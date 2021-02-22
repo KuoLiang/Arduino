0831826ba5aafc75c7302fa974f8c4e14eb78bc6
Kuo-Liang Ou <kloutw@gmail.com>
2021-02-23 00:14:18 +0800

    Update TrafficLight.ino

--- TrafficLight/TrafficLight.ino
+++ TrafficLight/TrafficLight.ino
@@ -1,25 +1,6 @@
 /*
-  Button
-
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
-
-  The circuit:
-  - LED attached from pin 13 to ground
-  - pushbutton attached to pin 2 from +5V
-  - 10K resistor attached to pin 2 from ground
-
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Button
+紅綠燈
+紅常暗，綠常亮
 */
 
 // constants won't change. They're used here to set pin numbers:
