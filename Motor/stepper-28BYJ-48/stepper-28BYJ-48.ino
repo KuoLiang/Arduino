2c772b8af1a9071293558524b896d2764f58f246
Kuo-Liang Ou <kloutw@gmail.com>
2021-04-23 22:42:33 +0800

    a

---
+++ Motor/stepper-28BYJ-48/stepper-28BYJ-48.ino
@@ -0,0 +1,20 @@
+#include <Unistep2.h>
+
+Unistep2 stepper(8, 9, 10, 11, 4096, 1000);// IN1, IN2, IN3, IN4, 總step數, 每步的延遲(in micros)
+
+void setup()
+{
+
+}
+
+void loop()
+{
+  stepper.run();  //步進機啟動
+
+  if ( stepper.stepsToGo() == 0 ){ // 還剩幾步沒走完
+    delay(500);
+    stepper.move(4096);    //正轉
+    //stepper.move(-4096);  //負數就是反轉
+  }
+
+}
