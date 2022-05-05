6db3c46473f0af9eff719d4ca8c8c2ec117622af
Kuo-Liang Ou <kloutw@gmail.com>
2022-05-05 16:16:55 +0800

    a

---
+++ ESP32/Wifi-ThingSpeak-DHT11/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "tcap"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "3838383838"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1722615			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "U3NCO8020XIQ8W1Q"   // replace XYZ with your channel write API Key
