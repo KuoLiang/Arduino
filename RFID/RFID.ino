d0d43701932aaaedeb27085869d50259e79dc885
Kuo-Liang Ou <kloutw@gmail.com>
2022-05-10 08:46:11 +0800

    Update RFID.ino

--- RFID/RFID.ino
+++ RFID/RFID.ino
@@ -1,18 +1,16 @@
 /*
- * 
  * All the resources for this project: http://randomnerdtutorials.com/
  * Modified by Rui Santos
- * d
  * Created by FILIPEFLOP
- * 
  */
 #include <SPI.h>
 #include <MFRC522.h>
 #include <ESP32Servo.h>
 #include "music.h"
  
-#define SS_PIN 5 //it is for select SDAorNSS; 18:SCK ; 23:MOSI ; 19:MISO
+#define SS_PIN 5 // pin5:SDAorNSS; 18:SCK ; 23:MOSI ; 19:MISO
 #define RST_PIN 32
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
  
 void setup() 
@@ -24,6 +22,7 @@ void setup()
   pinMode(12,OUTPUT); // for tone  無源
 
   mfrc522.PCD_Init();   // Initiate MFRC522
+  mfrc522.PCD_DumpVersionToSerial();
   Serial.println("Approximate your card to the reader...");
   Serial.println();
 
@@ -31,36 +30,30 @@ void setup()
 void loop() 
 {
   digitalWrite(LED_BUILTIN,LOW);
-  digitalWrite(13,LOW);
+  digitalWrite(13,LOW); //buzz keep silent
   
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  {
-    return;
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  // Look for new cards and read uid
+  if ( !mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) 
   {
     return;
   }
   //Show UID on serial monitor
   digitalWrite(LED_BUILTIN,HIGH);
-  digitalWrite(13,HIGH);
-  music();
+  digitalWrite(13,HIGH); //buzz only
+  music();               //do re me
   Serial.print("UID tag :");
   String content= "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  for (byte i = 0; i < mfrc522.uid.size; i++) // uid 轉換為文字變數 content
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
+  content.toUpperCase();
   Serial.println();
   Serial.print("Message : ");
-  content.toUpperCase();
-
 
   if (content.substring(1) == "99 A9 E4 6E" || content.substring(1) == "89 8D CE 6E") //change here the UID of the card/cards that you want to give access
   {
