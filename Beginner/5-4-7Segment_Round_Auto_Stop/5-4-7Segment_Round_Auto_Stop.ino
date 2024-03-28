c6e81a0f68f5d2c6f5b228b2e2e71b2b74b46291
Kuo-Liang Ou <kloutw@gmail.com>
2024-03-28 23:18:46 +0800

    a

--- Beginner/5-4-7Segment_Round_Auto_Stop/5-4-7Segment_Round_Auto_Stop.ino
+++ Beginner/5-4-7Segment_Round_Auto_Stop/5-4-7Segment_Round_Auto_Stop.ino
@@ -1,5 +1,5 @@
-const int buttonPin = 2; // 按鈕連接到pin 2
-const int leds[] = {3, 4, 5, 6, 7, 8, 9}; // 七段顯示器外圍LED的腳位
+const int buttonPin = 8; // 按鈕連接到pin 8
+const int leds[] = {2, 3, 4, 5, 6, 7}; // 七段顯示器外圍LED的腳位
 const int debounceDelay = 50; // 按鈕去抖動的延遲時間（毫秒）
 int buttonState; // 目前按鈕狀態
 int lastButtonState = LOW; // 上一次按鈕狀態
@@ -12,7 +12,7 @@ const long ledInterval = 100; // LED切換間隔時間（毫秒）
 
 void setup() {
   pinMode(buttonPin, INPUT);
-  for (int i = 0; i < 7; i++) {
+  for (int i = 0; i < 6; i++) {
     pinMode(leds[i], OUTPUT);
     digitalWrite(leds[i], HIGH); // 共陽型，初始化為關閉狀態
   }
@@ -33,7 +33,7 @@ void loop() {
         isOdd = pressCount % 2 != 0;
         if (!isOdd) {
           // 如果按壓次數為偶數，停止在當前LED
-          for (int i = 0; i < 7; i++) {
+          for (int i = 0; i < 6; i++) {
             digitalWrite(leds[i], i == currentLed ? LOW : HIGH); // 關閉所有LED，除了當前的
           }
         }
@@ -47,7 +47,7 @@ void loop() {
     // 關閉目前的LED
     digitalWrite(leds[currentLed], HIGH); 
     // 計算下一個LED的索引
-    currentLed = (currentLed + 1) % 7;
+    currentLed = (currentLed + 1) % 6;
     // 點亮下一個LED
     digitalWrite(leds[currentLed], LOW); 
     lastLedChangeTime = millis();
--- Beginner/7-blynk.ino/7-blynk.ino.ino
+++ Beginner/7-blynk.ino/7-blynk.ino.ino
@@ -1,79 +1,48 @@
-/*************************************************************
+int segments[] = {2, 3, 4, 5, 6, 7, 8}; // 定義連接到七段顯示器各段的 pin
+int buttonPin = 9; // 定義按鈕的 pin
+int lastButtonState = LOW; // 上次按鈕狀態，用於邊緣檢測
+unsigned long lastDebounceTime = 0;  // 上次變化時間
+unsigned long debounceDelay = 50;    // 防抖動延遲
 
-  This is a simple demo of sending and receiving some data.
-  Be sure to check out other examples!
- *************************************************************/
-
-/* Fill-in information from Blynk Device Info here */
-#define BLYNK_TEMPLATE_ID           "TMPL6V9jeCuzV"
-#define BLYNK_TEMPLATE_NAME         "Quickstart Template"
-#define BLYNK_AUTH_TOKEN            "Iz9zGWIFemM9F40i9kqPtqM6JIPuL8Xe"
-
-/* Comment this out to disable prints and save space */
-#define BLYNK_PRINT Serial
-
-
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-#include "secret.h"
-BlynkTimer timer;
-
-// This function is called every time the Virtual Pin 0 state changes
-BLYNK_WRITE(V0)
-{
-  // Set incoming value from pin V0 to a variable
-  int value = param.asInt();
-
-  // Update state
-  Blynk.virtualWrite(V1, value);
-  digitalWrite(2,value);
+void setup() {
+  randomSeed(analogRead(0)); // 初始化隨機數發生器
+  for(int i = 0; i < 7; i++){
+    pinMode(segments[i], OUTPUT); // 設定七段顯示器段落的 pins 為 OUTPUT
+  }
+  pinMode(buttonPin, INPUT); // 設定按鈕 pin 為 INPUT
 }
 
-// This function is called every time the device is connected to the Blynk.Cloud
-BLYNK_CONNECTED()
-{
-  // Change Web Link Button message to "Congratulations!"
-  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
-  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
-  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
-}
+void loop() {
+  int buttonState = digitalRead(buttonPin); // 讀取當前按鈕狀態
+  // 檢測按鈕邊緣
+  if (buttonState != lastButtonState) {
+    lastDebounceTime = millis(); // 重置最後變化時間
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // 如果按鈕狀態穩定超過防抖時間，則進行處理
+    if (buttonState == LOW && lastButtonState == HIGH) {
+      // 隨機選擇一個段落來點亮
+      int rndSegment = random(0, 7); // 生成一個在 0 到 6 之間的隨機數
+      int totalSteps = 7 * (1 + random(1, 4)); // 總步數 = 7段 * (1到3圈)
 
-// This function sends Arduino's uptime every second to Virtual Pin 2.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
-  int photoR_Value=analogRead(36);
-  Serial.println(photoR_Value);
-  Blynk.virtualWrite(V4, photoR_Value);
-}
+      for(int step = 0; step < totalSteps; step++){
+        int currentSegment = step % 7; // 當前要點亮的段落
 
-void setup()
-{
-  // Debug console
-  Serial.begin(115200);
+        // 先關閉所有段落
+        for(int j = 0; j < 7; j++){
+          digitalWrite(segments[j], HIGH);
+        }
+        
+        // 點亮當前段落
+        digitalWrite(segments[currentSegment], LOW);
 
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+        // 等待一段時間以觀察動畫效果
+        delay(100); // 可以根據需要調整這個延遲時間
+      }
 
-  // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
-  pinMode(2, OUTPUT); // Set GPIO 2 as output
-  pinMode(36, INPUT); // Set GPIO 2 as output
-}
-
-void loop()
-{
-  Blynk.run();
-  timer.run();
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
-
-}
-
-
+      // 最後確保隨機選擇的那個段落被點亮
+      for(int j = 0; j < 7; j++){
+        digitalWrite(segments[j], HIGH);
+      }
+      digitalWrite(segments[rndSegment], LOW);
+      delay(1000); // 讓最後選擇的段落保持
