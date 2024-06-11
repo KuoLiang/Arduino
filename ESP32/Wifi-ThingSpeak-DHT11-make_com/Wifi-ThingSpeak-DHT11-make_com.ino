51acc9c9a1506e05ffda24f0e023529256f9c1f1
Kuo-Liang Ou <kloutw@gmail.com>
2024-06-11 09:40:18 +0800

    Update Wifi-ThingSpeak-DHT11-make_com.ino

--- ESP32/Wifi-ThingSpeak-DHT11-make_com/Wifi-ThingSpeak-DHT11-make_com.ino
+++ ESP32/Wifi-ThingSpeak-DHT11-make_com/Wifi-ThingSpeak-DHT11-make_com.ino
@@ -1,12 +1,11 @@
-
+// 結合 ThingSpeak 之 Wifi Client 以及呼叫 make.com 之 Wifi HTTPClient
 #include <DHT.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ThingSpeak.h> 
 #include "../../../secrets.h"
-#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
 
-#define DHTPIN 4          // What digital pin we're connected to
-// Uncomment whatever type you're using!
+#define DHTPIN 4          // for DHT11
 #define DHTTYPE DHT11     // DHT 11
 //#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
 //#define DHTTYPE DHT21   // DHT 21, AM2301
@@ -14,11 +13,12 @@
 char ssid[] = SECRET_SSID;   // your network SSID (name) 
 char pass[] = SECRET_PASS;   // your network password
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
 WiFiClient  client;
 DHT dht(DHTPIN, DHTTYPE);
+
 unsigned long myChannelNumber = SECRET_CH_ID;
 const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
-int sensorValue=0;
 
 // Initialize our values
 int number1 = 0;
@@ -26,59 +26,62 @@ int number2 = random(0,100);
 int number3 = 0;
 int number4 = 0;
 String myStatus = "";
+
 //==========================
+//call the webhook of make.com
+//==========================
+
 void UploadData(){ 
-  HTTPClient http;  
-  String url1= url+"field1="+(int)sensorValue;
-  http.begin(url1);
-  int httpCode = http.GET();
+  HTTPClient myWebClient;  
+  String url_str= url+"field1="+(int)number1+"field2="+(int)number2;
+  myWebClient.begin(url_str);
+  int httpCode = myWebClient.GET();
   if(httpCode == HTTP_CODE_OK){
-    String payload = http.getString();
-    Serial.print("網頁內容=");
+    String payload = myWebClient.getString();
+    Serial.print("Webhook response : ");
     Serial.println(payload);
   } 
   else{
-     Serial.println("網路傳送失敗");
+     Serial.println("upload failed");
   }
-  http.end(); 
+  myWebClient.end(); 
 }
 
+//==========================
+//setup
+//==========================
 
 void setup() {
-  Serial.begin(115200);  //Initialize serial
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo native USB port only
-  }
   pinMode(BUILTIN_LED,OUTPUT);
-  dht.begin();
-  WiFi.mode(WIFI_STA);   
+  Serial.begin(115200); //Initialize serial
+  dht.begin();          //Initialize DHT
+  WiFi.mode(WIFI_STA);  //Initialize Wifi
   ThingSpeak.begin(client);  // Initialize ThingSpeak
 }
 
 void loop() {
-
   // Connect or reconnect to WiFi
   if(WiFi.status() != WL_CONNECTED){
-    Serial.print("Attempting to connect to SSID: ");
+    Serial.print("Connecting to SSID: ");
     Serial.println(SECRET_SSID);
     while(WiFi.status() != WL_CONNECTED){
       WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
       Serial.print(".");
-      delay(5000);     
+      delay(1000);     
     } 
     Serial.println("\nConnected.");
   }
 
 
   float h = dht.readHumidity();
-  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+  float t = dht.readTemperature(); // dht.readTemperature(true) for Fahrenheit
   Serial.println(h);
   Serial.println(t);
   // set the fields with the values
-  ThingSpeak.setField(1, number1);
-  ThingSpeak.setField(2, number2);
-  ThingSpeak.setField(3, h);
-  ThingSpeak.setField(4, t);
+  ThingSpeak.setField(1, number1); // 0 to 99
+  ThingSpeak.setField(2, number2); // random
+  ThingSpeak.setField(3, h);       // humidity
+  ThingSpeak.setField(4, t);       // temperature
 
   // figure out the status message
   if(number1 > number2){
@@ -90,7 +93,6 @@ void loop() {
   else{
     myStatus = String("field1 equals field2");
   }
-  
   // set the status
   ThingSpeak.setStatus(myStatus);
   
